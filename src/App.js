581326d2fe7387ff4facfb352c118eac9a9427e3
karenbarseghyan <karenbarseghyan90@gmail.com>
2020-11-23 15:11:40 +0400

    calculator styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React, {useState} from 'react'
-
+import  "./styles.css"
 
 function App() {
   const [data,setData] = useState("");
   const calcBtns = [];
-  [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0 , "."].forEach (item=>{
+  [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0 , ".", "%"].forEach (item=>{
     calcBtns.push(
       <button onClick = {event => {
         setData(data+event.target.value)
@@ -18,17 +18,20 @@ function App() {
   })
 
   return (
-    <div>
-      <div>{calcBtns}</div>
-      <div>{data}</div>
-      <button onClick = {()=> setData(data.substr(0,data.length - 1))}>Clear</button>
-      <button onClick = {() => setData ("")}>ac</button>
-      <button onClick = {event => setData(data + event.target.value)} value = "+">+</button>
-      <button onClick = {event => setData(data + event.target.value)} value = "-">-</button>
-      <button onClick = {event => setData(data + event.target.value)} value = "*">*</button>
-      <button onClick = {event => setData(data + event.target.value)} value = "/">/</button>
+    <div className = "wrapper">
+      <div className = "show-input">{data}</div>
+      <div className = "digits flex">{calcBtns}</div>
+      <div className = "modifiers subgrid">
+        <button onClick = {()=> setData(data.substr(0,data.length - 1))}>Clear</button>
+        <button onClick = {() => setData ("")}>ac</button>
+      </div>
+      <div className = "operations subgrid">
+        <button onClick = {event => setData(data + event.target.value)} value = "+">+</button>
+        <button onClick = {event => setData(data + event.target.value)} value = "-">-</button>
+        <button onClick = {event => setData(data + event.target.value)} value = "*">*</button>
+        <button onClick = {event => setData(data + event.target.value)} value = "/">/</button>
+      </div>
     </div>
   );
 }
-
 export default App;
